c3d64242a4ec697a8836b3799ed3f3380d8e3a16
theyadev <gtheya@gmail.com>
2022-01-21 16:20:03 +0100

    removed staffroles

--- aml/main/admin.py
+++ aml/main/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import StaffRole, Staff, Genre, Song, UserRole, User, Lists
+from .models import Staff, Genre, Song, UserRole, User, Lists
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
 
-admin.site.register(StaffRole)
 admin.site.register(Staff)
 admin.site.register(Genre)
 admin.site.register(Song)
--- aml/main/models.py
+++ aml/main/models.py
@@ -1,20 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class StaffRole(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=25)
-    description = models.TextField()
-
-    def __str__(self) -> str:
-        return self.name
-
 class Staff(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40)
     description = models.TextField()
     imageUrl = models.URLField()
-    role = models.ForeignKey(StaffRole, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.name
