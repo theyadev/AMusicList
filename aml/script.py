72238e26d895801932ffb7f8bc0e9352af9ead67
theyadev <gtheya@gmail.com>
2022-01-24 16:16:01 +0100

    spotipy script

--- aml/script.py
+++ aml/script.py
@@ -9,47 +9,77 @@ secret = "b18a56fe8b4840b0bda4b83a8d533fb5"
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+def importAlbumsFromPlaylist(playlist_id = "5S8SJdl1BDc0ugpkEvFsIL"):
+    playlist = sp.playlist(playlist_id)
+    for track in playlist['tracks']['items']:
+        importTracksFromAlbum(track['track']['album']['id'])
 
-playlist = sp.playlist("5S8SJdl1BDc0ugpkEvFsIL")
+def importTracksFromAlbum(album_id):
+    album = sp.album(album_id)
 
-for track in playlist['tracks']['items']:
-    song_name = track['track']['name']
-    image = track['track']['album']['images'][0]['url']
-    release_date = track["added_at"]
+    album_name = album['name']
+    album_release = album['release_date']
+    album_image = album['images'][0]['url']
 
-    artists = []
+    album_tracks = sp.album_tracks(album_id)
 
 
-    for artist in track['track']['artists']:
-        name: str = artist['name']
+    for track in album_tracks['items']:
+        song_name = track['name']
+        length = track['duration_ms'] // 1000
 
-        myStaff = Staff.objects.filter(name=name)
-        
-        if len(myStaff) > 0: 
-            artists.append(myStaff[0])
-            continue
-        
-        id: str = artist["id"]
-        spotify_link: str = artist['external_urls']['spotify']
-        
+        artists = []
 
-        artist_res = sp.artist(id)
+        for artist in track['artists']:
+            name: str = artist['name']
 
-        if artist_res['images'] != []: image: str = artist_res['images'][0]['url']
-        else: image = ""
+            myStaff = Staff.objects.filter(name=name)
+            
+            if len(myStaff) > 0: 
+                artist = myStaff[0]
+            else:
+                artist = importArtistFromDict(artist)
 
-        DESCRIPTION = f"{spotify_link}"
+            artists.append(artist)
 
-        newStaff = Staff(name= name, imageUrl=image, description=DESCRIPTION)
+        song = Song.objects.filter(title=song_name)
 
-        newStaff.save()
+        if len(song) == 0:
+            song = Song()
+        else:
+            song = song[0]
 
-        artists.append(newStaff)
-        
-    # newSong = Song(title=song_name, imageUrl=image, releaseDate=release_date, updatedDate=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    newSong = Song.objects.filter(title=song_name)[0]
+        song.title = song_name
+        song.imageUrl = album_image
+        song.release_date = album_release
+        song.album_name = album_name
+        song.length = length
 
-    for staff in artists:
-        newSong.staffs.add(staff)
+        song.save()
+
+        for staff in artists:
+            print(song)
+            if not song.staffs.filter(pk=artist.pk).exists():
+                song.staffs.add(staff)
+
+        song.save()
+
+def importArtistFromDict(artist_dict):
+    id: str = artist_dict["id"]
+    name: str = artist_dict['name']
+    spotify_link: str = artist_dict['external_urls']['spotify']
     
-    newSong.save()
+    artist_res = sp.artist(id)
+
+    if artist_res['images'] != []: image: str = artist_res['images'][0]['url']
+    else: image = ""
+
+    description = f"{spotify_link}"
+
+    newStaff = Staff(name=name, imageUrl=image, description=description)
+
+    newStaff.save()
+
+    return newStaff
+
+importAlbumsFromPlaylist("68agWSb6PQPIe8DUk0LNIP")
