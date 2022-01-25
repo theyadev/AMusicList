c44ac42b483a5e66168e42e7fd0b13c1cbc317ad
Fassi Théo <gtheya@gmail.com>
2022-01-25 17:15:47 +0100

    added add to list et add to favourite

--- aml/main/urls.py
+++ aml/main/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
-from . import views
+from .views import views
+from .views import api
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("song/<int:songId>", views.detail, name="detail"),
-    path("staff/<int:staffId>", views.staff, name="staff"),
-    path("logout", views.logout_view, name="logout"),
-    path("login", views.login_view, name="Login"),
-    path("signup", views.signup_view, name="Sign Up"),
+    path("song/<int:songId>", views.song, name="detail"),
+    path("artist/<int:staffId>", views.artist, name="staff"),
+    path("login", views.login, name="Login"),
+    path("signup", views.signup, name="Sign Up"),
+    path("api/logout", api.logout_api, name="logout"),
+    path("api/signup", api.signup_api, name="signup"),
+    path("api/login", api.login_api, name="login"),
+    path("api/add/<int:songId>", api.add_to_list, name="Add song to list"),
+    path("api/add/favourite/<int:songId>", api.add_to_favourite, name="Add song to fav"),
+
 ]
