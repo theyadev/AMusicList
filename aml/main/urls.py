a9e759168d034ada86c603bae5bb97223f5977db
Fassi Théo <gtheya@gmail.com>
2022-01-25 12:33:36 +0100

    changed path to song and staff

--- aml/main/urls.py
+++ aml/main/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:songId>', views.detail, name="detail"),
-    path('<int:songId>/staff', views.staff, name="staff"),
+    path('song/<int:songId>', views.detail, name="detail"),
+    path('staff/<int:staffId>', views.staff, name="staff"),
     path('logout', views.logout_view, name="logout"),
     path('login', views.login_view, name="Login"),
     path('signup', views.signup_view, name="Sign Up")
