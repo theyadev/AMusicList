97c37fbaca23df433695bcc6f9845f757c6bb5cd
Fassi Théo <gtheya@gmail.com>
2022-02-03 14:58:13 +0100

    added home

--- aml/users/urls.py
+++ aml/users/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.HomeView.as_view(), name="home"),
+    path("login", views.LoginView.as_view(), name="login"),
+    path("signup", views.SignupView.as_view(), name="signup"),
+    path("logout", views.LogoutView.as_view(), name="logout"),
     path("user/<int:pk>/", views.UserView.as_view(), name="user"),
-    path("login", views.LoginView.as_view()),
-    path("signup", views.SignupView.as_view()),
-    path("logout", views.LogoutView.as_view()),
     path("api/add/<int:songId>", views.AddToListView.as_view()),
     path("api/add/favourite/<int:songId>", views.AddToFavourite.as_view()),
 ]
