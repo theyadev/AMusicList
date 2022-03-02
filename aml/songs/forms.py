0867030ed704d66943a55d76339689521e02ea30
Fassi Théo <gtheya@gmail.com>
2022-03-02 15:36:47 +0100

    changed label

--- aml/songs/forms.py
+++ aml/songs/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class AddForm(forms.Form):
     """Form used to lookup songs/albums/playlists"""
 
-    query = forms.CharField()
+    query = forms.CharField(label="Recherche")
     type = forms.ChoiceField(
         choices=[("track", "Titre"), ("album", "Album"), ("playlist", "Playlist")]
     )
