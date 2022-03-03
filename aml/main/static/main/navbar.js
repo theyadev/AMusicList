47be60b604af32c326a7f8576052d45fe270e4c2
Fassi Théo <gtheya@gmail.com>
2022-03-03 16:49:20 +0100

    init

--- aml/main/static/main/navbar.js
+++ aml/main/static/main/navbar.js
@@ -6,3 +6,34 @@ if (popup.getElementsByClassName("popup__card").length === 0) popup_button.style
 popup_button.onclick = () => {
     popup.classList.toggle("popup--show")
 }
+
+const colors = {
+    "--primary": {
+        dark: "black",
+        light: "white"
+    },
+    "--secondary": {
+        dark: "gray",
+        light: "blue"
+    }
+}
+
+const checkbox = document.getElementById("darkmode")
+const darkMode = localStorage.darkMode === "true" || false
+
+checkbox.checked = darkMode 
+
+changeRootVariable(darkMode)
+
+checkbox.onclick = () => {
+    localStorage.darkMode = checkbox.checked
+    changeRootVariable(checkbox.checked)
+}
+
+function changeRootVariable(dark = false) {
+    const theme = dark ? "dark" : "light"
+
+    for (const key in colors) {
+        document.documentElement.style.setProperty(key, colors[key][theme]);
+    }
+}
