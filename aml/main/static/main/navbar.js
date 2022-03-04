1269bcc03e5420c26e2527d5526fbbf486477c98
theyadev <gtheya@gmail.com>
2022-03-04 11:55:30 +0100

    Added comments, docstrings

--- aml/main/static/main/navbar.js
+++ aml/main/static/main/navbar.js
@@ -1,39 +1,111 @@
-const popup_button = document.getElementById("popup-btn")
-const popup = document.getElementById("popup")
+const COLORS = {
+  "--primary": {
+    dark: "black",
+    light: "white",
+  },
+  "--secondary": {
+    dark: "white",
+    light: "black",
+  },
+  "--white-transparent-dark": {
+    dark: "rgba(0, 0, 0, 0.6)",
+    light: "rgba(255, 255, 255, 0.6)",
+  },
+  "--gray-transparent-dark": {
+    dark: "rgba(255, 255, 255, 0.8)",
+    light: "rgba(0, 0, 0, 0.8)",
+  },
+  "--gray-transparent": {
+    dark: "rgba(255, 255, 255, 0.1)",
+    light: "rgba(0, 0, 0, 0.1)",
+  },
+  "--gray-light": {
+    dark: "#161616",
+    light: "#f7f7f7",
+  },
+};
 
-if (popup.getElementsByClassName("popup__card").length === 0) popup_button.style.display = "none" 
+/**
+ * Changes CSS variables based on a colors object and the dark them state
+ * @param {object} colors
+ * @param {boolean} dark
+ * @returns {void}
+ */
+function changeCssVariables(colors, dark = false) {
+  // On transforme le booleen en string "dark" || "light"
+  const theme = dark ? "dark" : "light";
 
-popup_button.onclick = () => {
-    popup.classList.toggle("popup--show")
+  //   Pour chaque variable (clé) dans l'objet colors
+  for (const variable_name in colors) {
+    // On récupère les deux couleurs de la variable
+    const variable = colors[variable_name];
+    // On récupère la bonne couleur en fonction du thème
+    const color = variable[theme];
+
+    // On définis la variable dans notre page HTML
+    document.documentElement.style.setProperty(variable_name, color);
+  }
 }
 
-const colors = {
-    "--primary": {
-        dark: "black",
-        light: "white"
-    },
-    "--secondary": {
-        dark: "gray",
-        light: "blue"
-    }
+/**
+ * Add style (property: value) on every element in the DOM
+ * @param {string} property
+ * @param {string} value
+ * @returns {void}
+ */
+function addStyleToAllElements(property, value) {
+  const elements = document.querySelectorAll("*");
+
+  for (const element of elements) {
+    element.style.setProperty(property, value);
+  }
 }
 
-const checkbox = document.getElementById("darkmode")
-const darkMode = localStorage.darkMode === "true" || false
+function handleDarkTheme() {
+  // On récupère l'element de type checkbox
+  const checkbox = document.getElementById("darkmode");
 
-checkbox.checked = darkMode 
+  // On récupère le status du mode sombre dans le localStorage
+  // Si il n'y a pas de valeur on dit que c'est égal a false
+  const darkMode = localStorage.darkMode === "true" || false;
 
-changeRootVariable(darkMode)
+  // On définis le status de la checkbox
+  checkbox.checked = darkMode;
 
-checkbox.onclick = () => {
-    localStorage.darkMode = checkbox.checked
-    changeRootVariable(checkbox.checked)
+  //   On change les variables CSS
+  changeCssVariables(COLORS, darkMode);
+
+  //   On ajoute la transition sur tout les elements
+  setTimeout(() => {
+    addStyleToAllElements(
+      "transition",
+      "background-color ease-in 0.5s, color ease 0.5s"
+    );
+  }, 0);
+
+  // Quand on clique sur la checkbox
+  checkbox.onclick = () => {
+    // On change la valeur du mode sombre dans le localStorage
+    localStorage.darkMode = checkbox.checked;
+    // On change les variables CSS
+    changeCssVariables(COLORS, checkbox.checked);
+  };
 }
 
-function changeRootVariable(dark = false) {
-    const theme = dark ? "dark" : "light"
+function handleNotifications() {
+  const popup_button = document.getElementById("popup-btn");
+  const popup = document.getElementById("popup");
 
-    for (const key in colors) {
-        document.documentElement.style.setProperty(key, colors[key][theme]);
-    }
+  if (!popup) return;
+
+  const cards = popup.getElementsByClassName("popup__card");
+
+  if (cards.length === 0) popup_button.style.display = "none";
+
+  popup_button.onclick = () => {
+    popup.classList.toggle("popup--show");
+  };
 }
+
+handleDarkTheme();
+handleNotifications();
